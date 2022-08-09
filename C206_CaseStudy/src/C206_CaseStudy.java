60adc4c97547f1228c5b8a47c8e636070298826b
21019352 <21019352@myrp.edu.sg>
2022-08-09 23:51:07 +0800

    Added ability to exit back to Canteen Menu after exiting Sub-Menu

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -4,27 +4,27 @@ import java.util.ArrayList;
 public class C206_CaseStudy {
 
 	public static void main(String[] args) {
-		
-		
+
+
 		ArrayList<Foodcorder> foodList = new ArrayList<Foodcorder>();
 		ArrayList<Promotion> promotionList = new ArrayList<Promotion>();
 		ArrayList<Stall> stallList = new ArrayList<Stall>();
 		ArrayList<Ingredient> orderList = new ArrayList<Ingredient>();
 
-		
+
 		Foodcorder cc1 = new Foodcorder("CC001", "Chicken rice", 4);
 		Foodcorder cc2 = new Foodcorder("CC002", "Nasi Lemak", 4);
-		
+
 		Promotion pc1 = new Promotion("PC001", "breakfast happy hour", 3);
 		Promotion pc2 = new Promotion("PC002", "Coffee break time", 3);
-		
+
 		Stall st1 = new Stall("ST001","Ah Huat Chicken Rice");
 		Stall st2 = new Stall("ST002","Muhd Nasi Briyani");
-				
+
 		Ingredient ig1 = new Ingredient("ST001","Ah Huat Chicken Rice", "Green Onion", "08/08/2022", 10, 1.5, false);
 		Ingredient ig2 = new Ingredient("ST002","Muhd Nasi Briyani", "Tomato", "07/07/2022", 20, 2.0, false);
-		
-		
+
+
 		foodList.add(cc1);
 		foodList.add(cc2);
 		promotionList.add(pc1);
@@ -33,9 +33,9 @@ public class C206_CaseStudy {
 		stallList.add(st2);
 		orderList.add(ig1);
 		orderList.add(ig2);
-		
+
 		// Promotion
-		
+
 		int option = 0;
 
 		while (option != 5) {
@@ -43,10 +43,10 @@ public class C206_CaseStudy {
 			C206_CaseStudy.menu();
 			option = Helper.readInt("Enter an option > ");
 
-			
+
 			// PROMOTION OPTION
 			if (option == 1) {
-			
+
 				while (option != 5) {
 
 					C206_CaseStudy.promotions();
@@ -61,19 +61,19 @@ public class C206_CaseStudy {
 						// Add a new item
 						PromotionList.setHeader("ADD");			
 
-							Promotion cc = inputPromotion();
-							PromotionList.addPromotion(promotionList, cc);
-							System.out.println("Promotion added");
+						Promotion cc = inputPromotion();
+						PromotionList.addPromotion(promotionList, cc);
+						System.out.println("Promotion added");
 
-						} 
+					} 
 
-					
+
 					else if (option == 3) {
 						// Return item
 						PromotionList.setHeader("RETURN");				
 
-							String tag = Helper.readString("Enter promo tag > ");
-							PromotionList.returnPromotion(promotionList, tag);
+						String tag = Helper.readString("Enter promo tag > ");
+						PromotionList.returnPromotion(promotionList, tag);
 
 					} else if (option == 5) {
 						System.out.println("Bye!");
@@ -81,49 +81,51 @@ public class C206_CaseStudy {
 						System.out.println("Invalid option");
 					}
 				}	
+				option = 0;
+				
 			} 
-			
+
 			// FOOD OPTION
 			else if (option == 2) {
-				
+
 				while (option != 5) {
 
-				      C206_CaseStudy.food();
-				      option = Helper.readInt("Enter an option > ");
+					C206_CaseStudy.food();
+					option = Helper.readInt("Enter an option > ");
 
-				      if (option == 1) {
-				        // View all items
-				        C206_CaseStudy.viewAllFood(foodList);
+					if (option == 1) {
+						// View all items
+						C206_CaseStudy.viewAllFood(foodList);
 
 
-				      } else if (option == 2) {
-				        // Add a new item
-				        C206_CaseStudy.setHeader("ADD");      
+					} else if (option == 2) {
+						// Add a new item
+						C206_CaseStudy.setHeader("ADD");      
 
-				          Foodcorder cc = inputFood();
-				          C206_CaseStudy.addFoodcorder(foodList, cc);
-				          System.out.println("Food added");
+						Foodcorder cc = inputFood();
+						C206_CaseStudy.addFoodcorder(foodList, cc);
+						System.out.println("Food added");
 
-				        } 
+					} 
 
-				      
-				      else if (option == 3) {
-				        // Return item
-				        C206_CaseStudy.setHeader("RETURN");        
 
-				          String tag = Helper.readString("Enter food tag > ");
-				          C206_CaseStudy.returnFoodcorder(foodList, tag);
+					else if (option == 3) {
+						// Return item
+						C206_CaseStudy.setHeader("RETURN");        
 
-				      } else if (option == 5) {
-				        System.out.println("Bye!");
-				      } else {
-				        System.out.println("Invalid option");
-				      }
+						String tag = Helper.readString("Enter food tag > ");
+						C206_CaseStudy.returnFoodcorder(foodList, tag);
 
-				    }
-				
-		
-				
+					} else if (option == 5) {
+						System.out.println("Bye!");
+					} else {
+						System.out.println("Invalid option");
+					}
+
+				}
+				option = 0;
+
+
 			} 
 			//STALL OPTION
 			else if (option == 3) {
@@ -132,60 +134,63 @@ public class C206_CaseStudy {
 					option = Helper.readInt("Enter an option > ");
 					if (option == 1) {
 						C206_CaseStudy.viewAllStall(stallList);
-						
+
 					} else if (option == 2){
 						C206_CaseStudy.setHeader("ADD STALL");      
 
-				          Stall st = inputStall();
-				          C206_CaseStudy.addStall(stallList, st);
-				          System.out.println("Stall added");
-						
+						Stall st = inputStall();
+						C206_CaseStudy.addStall(stallList, st);
+						System.out.println("Stall added");
+
 					} else if (option == 3){
 						C206_CaseStudy.setHeader("DELETE STALL");        
 
-				          String id = Helper.readString("Enter Stall ID > ");
-				          C206_CaseStudy.deleteStall(stallList, id);
-						
+						String id = Helper.readString("Enter Stall ID > ");
+						C206_CaseStudy.deleteStall(stallList, id);
+
 					} else if (option == 5) {
 						System.out.println("Good bye!");
 						
 					} else {
 						System.out.println("Invalid Option");
-						
+
 					}
 				}
-			//ORDER OPTION
-			} else if (option == 4) {
+				option = 0;
 				
+				//ORDER OPTION
+			} else if (option == 4) {
+
 				while (option != 5) {
 					C206_CaseStudy.ingredientMenu();
 					option = Helper.readInt("Enter an option > ");
-					
+
 					if (option == 1) {
 						C206_CaseStudy.viewAllOrder(orderList);
-						
+
 					}else if (option == 2){
 						C206_CaseStudy.setHeader("ADD ORDER of INGREDIENT");      
 
-				          Ingredient or = inputOrder(stallList);
-				          C206_CaseStudy.addOrder(stallList,orderList, or);
-				          
+						Ingredient or = inputOrder(stallList);
+						C206_CaseStudy.addOrder(stallList,orderList, or);
+
 					}else if (option == 3){
 						C206_CaseStudy.setHeader("DELETE ORDER");        
 
-				          String stallId = Helper.readString("Enter Stall ID > ");
-				          String ingrName = Helper.readString("Enter Ingredient Name > ");
-				          C206_CaseStudy.deleteOrder(orderList, stallId, ingrName);
-				          
+						String stallId = Helper.readString("Enter Stall ID > ");
+						String ingrName = Helper.readString("Enter Ingredient Name > ");
+						C206_CaseStudy.deleteOrder(orderList, stallId, ingrName);
+
 					} else if (option == 5) {
 						System.out.println("Good bye!");
-						
+
 					} else {
 						System.out.println("Invalid Option");
-						
+
 					}
 				}
-				
+				option = 0;
+						
 			} else if (option == 5) {
 				System.out.println("Bye!");
 			} else {
@@ -193,7 +198,7 @@ public class C206_CaseStudy {
 				System.out.println("Invalid option");
 			}
 
-			
+
 		}
 
 	}
@@ -208,7 +213,7 @@ public class C206_CaseStudy {
 		Helper.line(80, "-");
 
 	}
-	
+
 	public static void promotions() {
 		PromotionList.setHeader("FOOD PROMOTION APP");
 		System.out.println("1. Display promotions");
@@ -218,7 +223,7 @@ public class C206_CaseStudy {
 		Helper.line(80, "-");
 
 	}
-	
+
 	public static void food() {
 		PromotionList.setHeader("FOOD MENU APP");
 		System.out.println("1. Display food");
@@ -229,7 +234,7 @@ public class C206_CaseStudy {
 		Helper.line(80, "-");
 
 	}
-	
+
 	public static void setHeader(String header) {
 		Helper.line(80, "-");
 		System.out.println(header);
@@ -253,10 +258,10 @@ public class C206_CaseStudy {
 		C206_CaseStudy.setHeader("FOOD LIST");
 		String output = String.format("%-10s %-30s %-10s\n", "FOOD TAG", "FOOD",
 				"PRICE");
-		 output += retrieveAllFood(foodList);	
+		output += retrieveAllFood(foodList);	
 		System.out.println(output);
 	}
-	
+
 	//================================= Option 1 View Promotions (CRUD- Read) =================================
 	public static String retrieveAllPromotion(ArrayList<Promotion> promotionList) {
 		String output = "";
@@ -273,7 +278,7 @@ public class C206_CaseStudy {
 		PromotionList.setHeader("PROMOTION LIST");
 		String output = String.format("%-10s %-30s %-10s\n", "PROMO TAG", "PROMOTION",
 				"PRICE");
-		 output += retrieveAllPromotion(promotionList);	
+		output += retrieveAllPromotion(promotionList);	
 		System.out.println(output);
 	}
 
@@ -287,15 +292,15 @@ public class C206_CaseStudy {
 
 		Foodcorder cc= new Foodcorder(tag, name, price);
 		return cc;
-		
+
 	}
 	public static void addFoodcorder(ArrayList<Foodcorder> foodList, Foodcorder cc) {
-		
+
 		foodList.add(cc);
-		
+
 	}
-	
-	
+
+
 	//================================= Option 2 Add promotion (CRUD - Create) =================================
 	public static Promotion inputPromotion() {
 		String tag = Helper.readString("Enter promo tag > ");
@@ -304,17 +309,17 @@ public class C206_CaseStudy {
 
 		Promotion cc= new Promotion(tag, name, price);
 		return cc;
-		
+
 	}
 	public static void addPromotion(ArrayList<Promotion> promotionList, Promotion cc) {
-		
+
 		promotionList.add(cc);
-		
+
 	}
-	
-	
-	
-	
+
+
+
+
 	//================================= Option 2 Return an item (CRUD - Update)=================================
 	public static boolean doReturnFoodcorder(ArrayList<Foodcorder> foodList,String tag) {
 		boolean isReturned = false;
@@ -323,29 +328,29 @@ public class C206_CaseStudy {
 			if (tag.equalsIgnoreCase(foodList.get(i).getAssetTag())
 					) {
 				foodList.get(i).setIsAvailable(true);
-				
+
 				isReturned = true;
-				
+
 			}
 		}
 		return isReturned;
-		
+
 	}
-	
+
 	public static void returnFoodcorder(ArrayList<Foodcorder> foodList, String tag) {
 		Boolean isReturned = doReturnFoodcorder(foodList, tag);
-		
+
 		if (isReturned == false) {
 			System.out.println("Invalid food tag");
-			
+
 		} else {
-		for (int i = 0; i < foodList.size(); i++) {
-			if (tag.equalsIgnoreCase(foodList.get(i).getAssetTag())
-					) {
-				foodList.remove(foodList.get(i));
+			for (int i = 0; i < foodList.size(); i++) {
+				if (tag.equalsIgnoreCase(foodList.get(i).getAssetTag())
+						) {
+					foodList.remove(foodList.get(i));
+				}
 			}
-		}
-		System.out.println("food " + tag + " deleted");
+			System.out.println("food " + tag + " deleted");
 		}
 
 	}
@@ -359,28 +364,28 @@ public class C206_CaseStudy {
 			if (tag.equalsIgnoreCase(promotionList.get(i).getAssetTag())
 					) {
 				promotionList.get(i).setIsAvailable(true);
-				
+
 				isReturned = true;
-				
+
 			}
 		}
 		return isReturned;
 	}
-	
+
 	public static void returnPromotion(ArrayList<Promotion> promotionList, String tag) {
 		Boolean isReturned = doReturnPromotion(promotionList, tag);
-		
+
 		if (isReturned == false) {
 			System.out.println("Invalid promo tag");
-			
+
 		} else {
-		for (int i = 0; i < promotionList.size(); i++) {
-			if (tag.equalsIgnoreCase(promotionList.get(i).getAssetTag())
-					) {
-				promotionList.remove(promotionList.get(i));
+			for (int i = 0; i < promotionList.size(); i++) {
+				if (tag.equalsIgnoreCase(promotionList.get(i).getAssetTag())
+						) {
+					promotionList.remove(promotionList.get(i));
+				}
 			}
-		}
-		System.out.println("promotion " + tag + " deleted");
+			System.out.println("promotion " + tag + " deleted");
 		}
 
 	}
@@ -404,59 +409,59 @@ public class C206_CaseStudy {
 		}
 		return output;
 	}
-	
+
 	public static void viewAllStall(ArrayList<Stall> stallList) {
 		C206_CaseStudy.setHeader("STALL LIST");
 		String output = String.format("%-10s %-10s\n", "STALL ID", "STALL NAME");
-		 output += retrieveAllStall(stallList);	
+		output += retrieveAllStall(stallList);	
 		System.out.println(output);
 	}
-	
+
 	public static Stall inputStall() {
 		String stId = Helper.readString("Enter stall ID > ");
 		String name = Helper.readString("Enter Stall Name> ");
 
 		Stall st = new Stall(stId, name);
 		return st;
-		
+
 	}
-	
+
 	public static void addStall(ArrayList<Stall> stallList, Stall st) {
-		
+
 		stallList.add(st);
-		
+
 	}
-	
+
 	public static boolean doDeleteStall(ArrayList<Stall> stallList,String id) {
 		boolean isDeleted = false;
 
 		for (int i = 0; i < stallList.size(); i++) {
 			if (id.equalsIgnoreCase(stallList.get(i).getStallId())) {
-				
+
 				isDeleted = true;
-				
+
 			}
 		}
 		return isDeleted;
 	}
-	
+
 	public static void deleteStall(ArrayList<Stall> stallList, String id) {
 		Boolean isDeleted = doDeleteStall(stallList, id);
-		
+
 		if (isDeleted== false) {
 			System.out.println("Stall ID not found.");
-			
+
 		} else {
-		for (int i = 0; i < stallList.size(); i++) {
-			if (id.equalsIgnoreCase(stallList.get(i).getStallId())) {
-				stallList.remove(stallList.get(i));
+			for (int i = 0; i < stallList.size(); i++) {
+				if (id.equalsIgnoreCase(stallList.get(i).getStallId())) {
+					stallList.remove(stallList.get(i));
+				}
 			}
-		}
-		System.out.println("Stall " + id + " deleted");
+			System.out.println("Stall " + id + " deleted");
 		}
 
 	}
-	
+
 	//================================= Option 4 Ingredient (CRUD - Update)=================================
 	public static void ingredientMenu() {
 		C206_CaseStudy.setHeader("Order Menu");
@@ -476,15 +481,15 @@ public class C206_CaseStudy {
 		}
 		return output;
 	}
-	
+
 	public static void viewAllOrder(ArrayList<Ingredient> orderList) {
 		C206_CaseStudy.setHeader("INGREDIENT LIST");
 		String output = String.format("%-10s %-25s %-25s %-20s %-10s %-10s %-10s\n", "STALL ID","STALL NAME", "INGREDIENT NAME",
 				"ORDER DATE", "QUATITY","PRICE","DELIVERED");
-		 output += retrieveAllOrder(orderList);	
+		output += retrieveAllOrder(orderList);	
 		System.out.println(output);
 	}
-	
+
 	public static Ingredient inputOrder(ArrayList<Stall> stallList) {
 		String stId = Helper.readString("Enter stall ID > ");
 		String stName = "";
@@ -493,7 +498,7 @@ public class C206_CaseStudy {
 				stName = stallList.get(i).getStallName();
 			}
 		}
-		
+
 		String name = Helper.readString("Enter Ingredient Name > ");
 		String orderDate = Helper.readString("Enter Date of Order > ");
 		int quantity = Helper.readInt("Enter Quantity > ");
@@ -502,12 +507,12 @@ public class C206_CaseStudy {
 
 		Ingredient or = new Ingredient(stId, stName, name, orderDate, quantity, price, isDelivered);
 		return or;
-		
+
 	}
-	
+
 	public static void addOrder(ArrayList<Stall> stallList, ArrayList<Ingredient> orderList, Ingredient or) {
 		boolean able = false;
-	
+
 		for (int i = 0; i < stallList.size(); i++) {
 			if (or.getStallId().equalsIgnoreCase(stallList.get(i).getStallId())) {
 				able = true;
@@ -519,42 +524,42 @@ public class C206_CaseStudy {
 		} else {
 			System.out.println("Order Cannot be Added as Stall does not exist");
 		}
-		
+
 	}
-	
+
 	public static boolean doDeleteOrder(ArrayList<Ingredient> orderList, String stallId, String ingrName) {
 		boolean isDeleted = false;
-//check for ingr name and stall
+		//check for ingr name and stall
 		for (int i = 0; i < orderList.size(); i++) {
 			if (stallId.equalsIgnoreCase(orderList.get(i).getStallId()) && ingrName.equalsIgnoreCase(orderList.get(i).getName())) {
-				
+
 				isDeleted = true;
-				
+
 			}
 		}
 		return isDeleted;
 	}
-	
+
 	public static void deleteOrder(ArrayList<Ingredient> orderList, String stallId, String ingrName) {
 		Boolean isDeleted = doDeleteOrder(orderList, stallId, ingrName);
 		String igName = "";
 		int igAmt = 0;
 		String stName = "";
-		
+
 		if (isDeleted == false) {
 			System.out.println("Stall and/or Order Name not found.");
-			
+
 		} else {
-			
+
 			for (int i = 0; i < orderList.size(); i++) {
 				if (stallId.equalsIgnoreCase(orderList.get(i).getStallId()) && ingrName.equalsIgnoreCase(orderList.get(i).getName())) {
 					igName = orderList.get(i).getName();
 					igAmt = orderList.get(i).getQuantity();
 					stName = orderList.get(i).getStallName();
 					orderList.remove(orderList.get(i));
-					
+
 				}
-				
+
 			}
 			System.out.println("Order of " + igAmt + " " + igName + " from" + stName + " deleted");
 		}
